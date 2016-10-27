504f8416c159f1b58074a3101847be3c97b82784
JessieLeehalo <jessielee1720@gmail.com>
2016-10-27 16:28:00 +0800

    update to the latest version

---
+++ MvcMusicStore/MvcMusicStore/Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+//using System.Data.Entity;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // The MusicDBContect class handles the task of connecting to the database and mapping Album objects to database records.
+    public class MusicDBContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+}
